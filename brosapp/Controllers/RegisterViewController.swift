5e92dffe0e32731f16b6dcaf2eb755c00b82e745
Shannon Brown <shabro4549@gmail.com>
2021-07-04 13:35:25 -0700

    Add feedback for registration
    Add feedbackLabel to RegistrationViewController to tell user what to change if registration goes wrong

--- brosapp/Controllers/RegisterViewController.swift
+++ brosapp/Controllers/RegisterViewController.swift
@@ -14,6 +14,7 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var feedbackLabel: UILabel!
     
     let db = Firestore.firestore()
     
@@ -23,24 +24,42 @@ class RegisterViewController: UIViewController {
         registerButton.layer.cornerRadius = registerButton.frame.height/2
         registerButton.clipsToBounds = true
         self.tabBarController?.tabBar.isHidden = true
+        feedbackLabel.alpha = 0
     }
     @IBAction func registerPressed(_ sender: Any) {
         if let email = emailTextField.text, let password = passwordTextField.text {
-            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-                if let e = error {
-                    print(e)
-                } else {
-                    self.performSegue(withIdentifier: "RegisterToAccount", sender: self)
-                    
-                }
+            if password.count < 6 {
+                feedbackLabel.alpha = 1
+                feedbackLabel.text = "Password must be more than 6 characters"
+            }
+            if nameTextField.text == "" {
+                nameTextField.placeholder = "Please enter name"
+            }
+            if emailTextField.text == "" {
+                emailTextField.placeholder = "Please enter an email"
+            }
+            if passwordTextField.text == "" {
+                passwordTextField.attributedPlaceholder = NSAttributedString(string: "Password Required", attributes: [NSAttributedString.Key.foregroundColor: UIColor.red])
             }
             
+            if password.count > 6 && nameTextField.text != "" && emailTextField.text != "" && passwordTextField.text != "" {
+                Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                    if let e = error {
+                        print(e)
+                        self.feedbackLabel.alpha = 1
+                        self.feedbackLabel.text = "Something went wrong, check email"
+                    } else {
+                        self.db.collection("users").document(self.emailTextField.text!).setData([
+                            "Name" : self.nameTextField.text!,
+                            "Email" : self.emailTextField.text!
+                        ])
+                        self.performSegue(withIdentifier: "RegisterToAccount", sender: self)
+                        
+                    }
+                }
+                
+            }
         }
-        
-        db.collection("users").document(emailTextField.text!).setData([
-            "Name" : nameTextField.text!,
-            "Email" : emailTextField.text!
-        ])
     }
 
 }
